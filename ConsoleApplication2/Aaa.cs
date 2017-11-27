5a75248265c9e722ceca23749d696a1537947ee9
Andrii1992 <super.80966898912@yandex.ru>
2017-11-27 16:35:48 +0100

    Add files via upload

---
+++ ConsoleApplication2/Aaa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Aaa
+    {
+
+     public virtual void Metoda(){
+
+           Console.WriteLine("Aaa");
+
+       }
+
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aaa a = new Aaa();
+            a.Metoda();
+            Bbb b = new Bbb();
+            b.Metoda();
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
